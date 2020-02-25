566e28cd6837a3f6e9954a9e053d29d5f7ba8b6c
thor <thor@cphbusiness.dk>
2020-02-25 11:10:39 +0100

    opgaver uge9

---
+++ Oops.java
@@ -0,0 +1,26 @@
+/*
+ *
+ * Hvis du kompilerer programmet får du 7 fejl.
+ * Kan du fixe programmet så det kan køre og
+ * outputtet bliver:
+ * "7 is the smallest!"
+ *
+ * 
+ */
+public class Oops {
+  public static void main(String[] args) {
+    int a = 7, b = 42;
+    minimum(a,b);
+    if {smaller == a} {
+      System.out.println("a is the smallest!");
+    }
+  }
+  public static void minimum(int a, int b) {
+    if (a < b) {
+      int smaller = a;
+    } else (a >= b) {
+      int smaller = b;
+    }
+    return int smaller;
+  }
+}
