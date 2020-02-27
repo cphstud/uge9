7a00c81eaf51bf11b3382e14387bb56b57c434be
thor <thor@cphbusiness.dk>
2020-02-27 13:29:43 +0100

    drawbox løsning

--- DrawBox.java
+++ DrawBox.java
@@ -1,28 +1,17 @@
 class DrawBox {
   public static void main(String[] args) {
     drawBox(6,10);
+
     /*
-     * FORMÅL: 
-     * for-loops samt parametrisere printf
-     * dvs at System.out.printf("%14s%n","#") bliver til
-     * format = "%" + variabel + "s%n";
-     * myChar = "*"
-     * System.out.printf(format,myChar);
-     * 
-     *
-     * OPGAVEN:
-     * tegn en firkant vha  metoden drawBox hvor højde og bredde 
-     * angives som parametre 
-     *
      * TESTKØRSEL:
      * drawBox(6,10) giver flg. figur
 
-        **********
-        *        *
-        *        *
-        *        *
-        *        *
-        **********
+     **********
+     *        *
+     *        *
+     *        *
+     *        *
+     **********
 
 */
 
@@ -30,13 +19,25 @@ class DrawBox {
   public static void drawBox(int height, int width) {
     String format = "";
     String myEmpty = " ";
+    String myCahr = "*";
     writeChars('*',width);
     // TODO: skriv for-loop hvor hver iteration tegner en linje
     // og hvor antallet af loops begrænses af height samt at
     // width indgår i formateringen af printf.
+    for (int i=2;i<height;i++) {
+      format = "%s%"+width+"s%n";
+      System.out.printf(format,myCahr,myCahr);
+      //System.out.printf("%s%10s%n",myCahr,myCahr);
+      //System.out.println("*      *");
+    }
+    writeChars('*',width);
 
   }
   public static void writeChars(char myChar, int frequency) {
     //TODO: skriv for-loopet der printer myChar 
+    for(int i=0;i<frequency+1;i++) {
+      System.out.print(myChar);
+    }
+    System.out.println();
   }
 }
